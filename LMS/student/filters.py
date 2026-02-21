a8ea0913a6c69d9e5fdb9b59116ce08e988bd25e
Cahal Agarwalla <cahalagarwalla333@gmail.com>
2026-02-21 13:53:17 +0530

    filter for char file

--- LMS/student/filters.py
+++ LMS/student/filters.py
@@ -4,6 +4,8 @@ from .models import Student
 class StudentFilter(django_filters.FilterSet):
     branch=django_filters.CharFilter(field_name="branch",lookup_expr="iexact")
     name=django_filters.CharFilter(field_name="name",lookup_expr="icontains")
+    student_id=django_filters.RangeFilter(field_name="student_id")
+
 
 
 
